d0428b560c2a46847b55f84e08d01731e7044d71
Janic <68118279+masterjanic@users.noreply.github.com>
2022-05-08 17:18:18 +0200

    feat: add meta tags
    Signed-off-by: Janic <68118279+masterjanic@users.noreply.github.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,24 @@
 import { defineNuxtConfig } from 'nuxt';
 
 export default defineNuxtConfig({
-  head: {
-    meta: [
-      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
-      { name: 'robots', content: 'index,follow' },
-      { name: 'theme-color', content: '#5865F2' },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+      meta: [
+        { name: 'description', content: 'A fast, web-based Discord token checker.' },
+        { name: 'keywords', content: 'Discord, Discord Tokens, Token Stealer, Token Checker, Discord Tools' },
+        { name: 'robots', content: 'index, follow' },
+        { name: 'theme-color', content: '#5865F2' },
+        { property: 'og:title', content: 'Online Discord Token Checker' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:site_name', content: 'Discord Token Checker' },
+        { property: 'og:description', content: 'A fast, web-based Discord token checker.' },
+        { name: 'twitter:title', content: 'Online Discord Token Checker' },
+        { name: 'twitter:description', content: 'A fast, web-based Discord token checker.' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
   },
   modules: ['@nuxtjs/color-mode', '@nuxtjs/tailwindcss', '@unocss/nuxt'],
   colorMode: {

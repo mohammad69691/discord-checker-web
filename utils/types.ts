6151e9dfe4b96e5ca43203e1545f7f559add4b0a
Janic <68118279+masterjanic@users.noreply.github.com>
2022-06-19 12:08:47 +0200

    chore: complete DiscordUser type
    Signed-off-by: Janic <68118279+masterjanic@users.noreply.github.com>

--- utils/types.ts
+++ utils/types.ts
@@ -6,11 +6,23 @@ export type DiscordUser = {
   id: string;
   username: string;
   discriminator: number;
-  avatar: string;
-  flags: number;
-  email: string;
-  phone: string;
-  verified: boolean;
+  avatar?: string;
+  bot?: boolean;
+  system?: boolean;
+  mfa_enabled?: boolean;
+  locale?: string;
+  flags?: number;
+  public_flags?: number;
+  purchased_flags?: number;
+  banner?: string;
+  banner_color?: string;
+  accent_color?: number;
+  bio?: string;
+  nsfw_allowed?: boolean;
+  email?: string;
+  phone?: string;
+  verified?: boolean;
+  premium_type?: number;
 };
 
 export type DiscordAccount = {
